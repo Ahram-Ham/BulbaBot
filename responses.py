ffcc184dd07bc1f500cd1848889a6b7fa155768c
Ahram-Ham <ahramham2001@gmail.com>
2023-09-07 23:52:29 -0700

    prepping for refactor

--- responses.py
+++ responses.py
@@ -14,6 +14,7 @@ def handle_response(message) -> str:
         return "Type in a Pokemon's ability.\nUsing an exclamation mark before the ability name will result in the response being returned to the current channel.\n" \
                          "Spaces are ok and the command isn't case-sensitive."
 
+
     # Add ability to a list
     ability = [p_message]
 
@@ -44,3 +45,27 @@ def handle_response(message) -> str:
         return_message = "Ability doesn't exist or is spelt wrong. Please enter in another."
 
     return return_message
+
+
+def write_txt():
+    p_message = message.lower()
+
+    URL = "https://bulbapedia.bulbagarden.net/wiki/Ability"
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.content, 'html5lib')  # If this line causes an error, run 'pip install html5lib' or install html5lib
+
+    ability_table = soup.find('table', attrs={'class': 'roundy'})
+    abilities = {}
+
+    # Parse through ability table and extract the ability name and text.
+    for row in ability_table.find_all('tr'):
+        columns = row.find_all('td')
+
+        if len(columns) >= 2:
+            ability_name = columns[1].text.strip()
+
+        file_object = open("text_files/abilities.txt", "w+")
+        file_object.write(ability_name + "\n")
+        file_object.close()
+
+
